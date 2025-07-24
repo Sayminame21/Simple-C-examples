010f91b8149ab2411b7f87c1ec90a467b45bd053
Emmanuel Henry Michelo <Emichelo21@icloud.com>
2025-07-24 22:55:02 +0200

    Add files via upload

---
+++ TotalAverage1.c
@@ -0,0 +1,23 @@
+/*computes the total and average of five numbers.*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    int total [5]; /*Array is created*/
+    int Total;
+    int average;
+
+    total[0] = 2;
+    total[1] = 4;
+    total[2] = 6;
+    total[3] = 8;
+    total[4] = 10;
+
+    Total = total[0] + total[1] + total[2] + total[3] + total[4]; /*Array numbers*/
+    average = Total /5;
+
+    printf("total is %d\tand the average is %d\t", Total,average); /*display output*/
+
+    return (0);
+}
