9eb2eefa00c7ce50483d4c3c9e8bb32d6f1cdaf7
Emmanuel Henry Michelo <Emichelo21@icloud.com>
2025-07-25 12:33:37 +0200

    Add files via upload

--- TotalAverage1.c
+++ TotalAverage1.c
@@ -2,20 +2,22 @@
 
 #include <stdio.h>
 
- int total [5]; /*Array is created*/
- int Total;
- int average;
-
 int main(void)
 {
-   
+    int total [5] = {2,4,6,8,12}; 
+    int Total;
+    int average;
 
+    /*int total[5];
     total[0] = 2;
     total[1] = 4;
     total[2] = 6;
     total[3] = 8;
-    total[4] = 10;
+    total[4] = 12; can also be used to initialize the array
+    but it is not recommended as it is less readable.
+    */
 
+  
     Total = total[0] + total[1] + total[2] + total[3] + total[4]; /*Array numbers*/
     average = Total /5;
 
