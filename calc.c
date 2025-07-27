573a31d75d37a8d5609eb09524ff05afb6573454
Emmanuel Henry Michelo <Emichelo21@icloud.com>
2025-07-27 22:04:53 +0200

    Add files via upload

---
+++ calc.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <string.h>
+
+char line[100];
+long int value;
+long int value_2;
+char operator;
+long int result;
+
+int main(void){
+	result= 0;
+	printf("The result is :%ld\n",value); //the starting point
+	printf("insert value and operator:\n"); //display input message
+	fgets(line,sizeof(line),stdin); // input value
+	sscanf(line,"%ld%c",&value,&operator);
+
+	printf("insert value\n"); //display input message
+	fgets(line,sizeof(line),stdin); // input value
+	sscanf(line,"%ld",&value_2);
+
+	while(1){
+		if(operator == '+'){ //used to input the addition operator
+			result=value+value_2;
+			printf("the result is: %ld\n",result);
+		}
+			if(operator == '-'){ //used to input the subtraction operator 
+			result=value-value_2;
+			printf("the result is:%ld\n",result);
+		}
+			if(operator == '*'){ //used to input the multiplication operator
+			result=value*value_2;
+			printf("the result is :%ld\n",result);
+		}
+			if(operator == '/'){ //used to input the division operator
+				if(value == 0 & value_2 == 0){
+					printf("error detected");
+				}
+			result=value/value_2;
+			printf("the result is %d\n",result);
+		}
+
+		return (0);
+	}
+
+
+
+
+
+
+}
